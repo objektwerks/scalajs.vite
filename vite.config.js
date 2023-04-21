0d05db3133af64f0db0543aea2e9719391efcb03
objektwerks <objektwerks@runbox.com>
2023-04-21 17:40:31 -0400

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
-  plugins: [scalaJSPlugin()],
-  base: '/target/public'
+  plugins: [scalaJSPlugin()]
 });
